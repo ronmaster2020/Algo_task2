32326660067b13bb6610e98dbb0ad2169175125b
ron <ron.ochak@gmail.com>
2025-01-06 16:52:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# student1_name student1_id
+# student2_name student2_id
+
+# The main execution:
+def main():
+    return
+
+# |-------------------------------------------------------|
+# |============= FUNCTION'S IMPLEMENTATION ===============|
+# |-------------------------------------------------------|
